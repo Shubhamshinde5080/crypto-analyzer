2d7c475422a04aa9f161239963bdc08e9eb60259
Shubham Shinde <bcs_2024065@iiitm.ac.in>
2025-07-09 17:54:01 +0530

    chore: fix catch type for CoinList

---
+++ src/lib/format.ts
@@ -0,0 +1,7 @@
+export const fmtUSD = (n: number) =>
+  new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 4,
+    maximumFractionDigits: 4,
+  }).format(n);
